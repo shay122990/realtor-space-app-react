503f0db99499d4cb0bc5ec306f55404af9114639
Shay.Asanova <shay.asanova@gmail.com>
2023-06-14 15:17:10 +0400

    created routes and linked in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/home.component";
+import Profile from "./routes/profile.component";
+import SignIn from "./routes/signIn.component";
+import SignUp from "./routes/signUp.component";
+import ForgotPassword from "./routes/forgotPassword.component";
+import Offers from "./routes/offers.component";
+
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-2xl'>realtor app</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
